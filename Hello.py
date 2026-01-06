8cc56d024438253a9dad1bc2ab4d866e90a2f53e
Owen Hendrix <owen.hendrix@gmail.com>
2026-01-06 16:31:04 -0500

    added command args

--- Hello.py
+++ Hello.py
@@ -1,1 +1,16 @@
-print("Hello, World")
+import sys
+
+def factorial_iterative(n):
+    if n < 0:
+        raise ValueError("Factorial is not defined for negative integers")
+    result = 1
+    for i in range(1, n+1):
+        result *= i
+    return result
+
+print(__name__)
+if __name__ == "__main__":
+    print(sys.orig_argv[2])
+    num  = int(sys.orig_argv[2])
+    print(f"{factorial_iterative(num)}")
+
